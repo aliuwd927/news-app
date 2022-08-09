45137b6689a7289eb602eef7622c4a003dd75659
Andy Liu <aliu927@gmail.com>
2022-08-09 01:36:40 -0700

    added redux

--- src/search/SearchBar.tsx
+++ src/search/SearchBar.tsx
@@ -1,9 +1,33 @@
+import React from "react";
+
+import { useDispatch } from "react-redux";
+import { updateSearch } from "./searchSlice";
+
 export default function SearchBar() {
+  const dispatch = useDispatch();
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    let grabSerchValue = new FormData(event.currentTarget);
+    //Search Bar takes the value of the item we are trying to search on the news website.
+    let searchBarValue = grabSerchValue
+      .get("searchBarValue")
+      ?.toString()
+      .trim();
+    console.log(searchBarValue);
+    dispatch(updateSearch(searchBarValue || ""));
+  }
+
   return (
     <div className="header_searchBar">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          <input type="text" aria-label="Search" placeholder="Search" />
+          <input
+            name="searchBarValue"
+            type="text"
+            aria-label="Search"
+            placeholder="Search"
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -23,5 +47,17 @@ export default function SearchBar() {
       </form>
 
 
+
+      useEffect?
+
+
+
+      Question:
+      As you search, do we provide a list of websites? --> Answer: We will provide a list of websites in which we will scrape ourselves
+      If yes, we need to scrape for list of websites. --> Bc we do have to provide it a list, we will list the websites in object. See Reducers in Redux.
+                                                      --> Think about RSS, Check SiteMaps. 
+      Then figure out if all websites have a common theme.
+      Use that common theme to pull data.
+      
 
 */
