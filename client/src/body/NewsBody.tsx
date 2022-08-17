314b6f530c1938d55213d28d39fa8eda85d6165b
Adam Kaplan <7332764+Adam-Kaplan@users.noreply.github.com>
2022-08-17 02:39:10 -0700

    code updates and comments

--- client/src/body/NewsBody.tsx
+++ client/src/body/NewsBody.tsx
@@ -1,13 +1,19 @@
 import type { RootState } from "../store";
 import { useSelector } from "react-redux";
+import { useGetSearchKeywordQuery } from "../service/searchApi";
 
 export default function NewsBody() {
-  const displayState = useSelector((state: RootState) => state.search.value);
-  //console.log(displayState);
+  // change to keyword, since that is what is set from <SearchBar>
+  const displayState = useSelector((state: RootState) => state.search.keyword);
+
+  // We use the query in the location where we will use the data.
+  const { data, isSuccess, isLoading } = useGetSearchKeywordQuery(displayState);
+  
   return (
     <div className="newsBody">
+      {isLoading && <div>Loading search "{displayState}"</div>}
       <div>
-        <div dangerouslySetInnerHTML={{ __html: displayState.join("") }}></div>
+        {isSuccess && data.map((tag: string, index: number) => <article key={index} dangerouslySetInnerHTML={{__html: tag}} />)}
       </div>
     </div>
   );
